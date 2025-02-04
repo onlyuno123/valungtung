80a62955de1dc8079590dcf6bfcac71b15346fe9
onlyuno123 <mkhoisakuku@gmail.com>
2025-02-05 00:38:31 +0700

    script.js

---
+++ script.js
@@ -0,0 +1,27 @@
+const messages = [
+    "Em chắc chứ!!!!!🤔",
+    "Thật không????😕",
+    "Có chắc chắn chưa!!!!",
+    "Shawty please...😭😭",
+    "Hãy suy nghĩ lại đii!",
+    "Nếu em nói không, anh sẽ rất buồn....🙁",
+    "Cực kỳ buồn",
+    "Đau lòng....💔",
+    "Được rồi, anh sẽ không nói nữa....",
+    "Đùa thôi, nói đồng ý đi mà ❤️
+];
+
+let messageIndex = 0;
+
+function handleNoClick() {
+    const noButton = document.querySelector('.no-button');
+    const yesButton = document.querySelector('.yes-button');
+    noButton.textContent = messages[messageIndex];
+    messageIndex = (messageIndex + 1) % messages.length;
+    const currentSize = parseFloat(window.getComputedStyle(yesButton).fontSize);
+    yesButton.style.fontSize = `${currentSize * 1.5}px`;
+}
+
+function handleYesClick() {
+    window.location.href = "yes_page.html";
+}
